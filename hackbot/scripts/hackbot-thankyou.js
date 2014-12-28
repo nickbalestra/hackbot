656b43729c7fb86919b213649a75ed1f1068e24a
Nick Balestra <nick@beyounic.com>
2014-12-28 14:24:47 +0100

    scripts cleanup

--- hackbot/scripts/hackbot-thankyou.js
+++ hackbot/scripts/hackbot-thankyou.js
@@ -1,15 +1,13 @@
 define(function () {
 
-    var response;
-    response = ["you're welcome", "no problem", "not at all", "don’t mention it", "it’s no bother", "it’s my pleasure", "my pleasure", "it’s all right", "it’s nothing", "think nothing of it", "sure", "sure thing"];
+    var response = ["you're welcome", "no problem", "not at all", "don’t mention it", "it’s no bother", "it’s my pleasure", "my pleasure", "it’s all right", "it’s nothing", "think nothing of it", "sure", "sure thing"];
 
     robot.respond(/(thank).*/i, function(msg) {
         return msg.send(msg.random(response));
     });
 
     return robot.hear(/thanks (.*)/i, function(msg) {
-        var name;
-        name = msg.match[1];
+        var name = msg.match[1];
         if (name.toLowerCase() === robot.name.toLowerCase()) {
             return msg.send(msg.random(response));
         }

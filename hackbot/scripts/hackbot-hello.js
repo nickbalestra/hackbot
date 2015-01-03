ac3db7960c673ba2046872ef054f0a577a7b6cce
Nick Balestra <nick@beyounic.com>
2015-01-03 21:11:36 +0100

    little improvements

--- hackbot/scripts/hackbot-hello.js
+++ hackbot/scripts/hackbot-hello.js
@@ -2,10 +2,10 @@ define(function () {
 
     //robot.command("hello", "reply with some greetings");
 
-    var hellos = ["Well hello there, %", "Hey %, hello!", "Good day, %", "Good 'aye!, %"],
+    var hellos = ["Well hello there, %. Type 'hackbot help' if you need anthing.", "Hey %, hello! Type 'hackbot help' if you need", "Good day, %. Type 'hackbot help' if you need", "Good 'aye!, %. Type 'hackbot help' if you need me"],
         mornings = ["Good morning, %", "Good morning to you too, %", "Marnin', %", "Good 'aye!, %"];
 
-    robot.hear(/(hi|hey|hello|good( [d'])?ay(e)?)/i, function(msg) {
+    robot.hear(/(hi|hey|nobody|hello|good( [d'])?ay(e)?)/i, function(msg) {
         var hello = msg.random(hellos);
         return msg.send(hello.replace("%", msg.message.user.name));
     });

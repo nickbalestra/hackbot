fb6c7bcd9e5e630aed24359fa35768a093beedd7
Nick Balestra <nick@beyounic.com>
2015-01-05 12:16:55 +0100

    User docs

--- hackbot/user.js
+++ hackbot/user.js
@@ -1,11 +1,24 @@
 define(function () {
 
-    // Represents an incoming message from the chat.
+    // ## User
+    // `new User(name, options)`
     //
-    // user - A User that sent the message.
+    // _Represents a participating user in the chat._
+    //
+    // **name** - A String of the user name.
+    //
+    // **options** - An optional Hash of key, value pairs for this user.
     User = (function() {
         function User(name, options) {
             this.name = name;
+            if (options == null) {
+                // TODO: for loop below can be moved in here
+                options = {};
+            }
+            for (k in options || {}) {
+                this[k] = options[k];
+            }
+            // TODO: Add msgHistory using options and remove it from being hardcoded in here
             this.msgHistory = [];
         }
 
